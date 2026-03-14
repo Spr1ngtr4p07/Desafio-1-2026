a5dc5270bb875a85f479b84b52e6ac60dde7bdb3
didier <didie@gmail.com>
2026-03-14 18:41:30 -0500

    Se agregael inicio del codigo con ejemplo

---
+++ Desafio1-2026/Desafiolibreria.h
@@ -0,0 +1,15 @@
+#ifndef DESAFIOLIBRERIA_H
+#define DESAFIOLIBRERIA_H
+
+#include <iostream>
+
+using namespace std;
+
+int suma();
+int resta();
+int division();
+
+
+#endif // DESAFIOLIBRERIA_H
+
+
