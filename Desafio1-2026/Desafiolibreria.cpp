db88b9b8d2022a6ec1db696b3e018cce8633a9cb
didier <didie@gmail.com>
2026-03-15 20:19:12 -0500

    Se agrega el prototipo de crear el tablero y imprimirlo

--- Desafio1-2026/Desafiolibreria.cpp
+++ Desafio1-2026/Desafiolibreria.cpp
@@ -3,15 +3,52 @@
 #include "Desafiolibreria.h"
 
 
-int suma() {
-    cout << "se invoca la funcion suma " << endl;
-    return 0;
+using namespace std;
+
+// Funcion que crea el tablero con memoria dinamica
+int* crearTablero(int alto)
+{
+    // Se reserva memoria para el tablero
+    // Cada posicion del arreglo representa una fila
+    int* tablero = new int[alto];
+
+    // Se recorren todas las filas
+    for(int i = 0; i < alto; i++)
+    {
+        // Se inicializa cada fila en 0
+        tablero[i] = 0;
+    }
+
+    // Se devuelve el puntero al tablero
+    return tablero;
+
 }
 
-int resta() {
-    return 0;
-}
+//  imprime el tablero
+void imprimirTablero(int* tablero, int ancho, int alto)
+{
+    // Recorre cada fila del tablero
+    for(int i = 0; i < alto; i++)
+    {
+        // Recorre cada columna de la fila
+        for(int j = ancho - 1; j >= 0; j--)
+        {
+            // Se revisa si el bit j esta encendido
+            // Ejemplo: si j = 3 -> 00001000
+            if(tablero[i] & (1 << j))
+            {
+                // Si el bit esta encendido
+                cout << "#";
+            }
+            else
+            {
+                // Si el bit esta apagado
+                cout << ".";
+            }
+        }
 
-int division() {
-    return 0;
+        // Salto de linea para imprimir la siguiente fila
+        cout << endl;
+    }
+
 }
--- Desafio1-2026/Desafiolibreria.h
+++ Desafio1-2026/Desafiolibreria.h
@@ -1,14 +1,23 @@
 #ifndef DESAFIOLIBRERIA_H
 #define DESAFIOLIBRERIA_H
+/*
+Esta libreria sirve para.............
+*/
 
 #include <iostream>
 
 using namespace std;
 
-int suma();
-int resta();
-int division();
+// Funcion que crea el tablero en memoria dinamica
+// Recibe el alto (numero de filas)
+// Devuelve un puntero al tablero creado
+int* crearTablero(int alto);
 
+// Funcion que imprime el tablero en consola
+// tablero -> arreglo que guarda las filas
+// ancho -> numero de columnas
+// alto -> numero de filas
+void imprimirTablero(int* tablero, int ancho, int alto);
 
 #endif // DESAFIOLIBRERIA_H
 
