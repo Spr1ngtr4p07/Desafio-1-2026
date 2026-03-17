0bfc0b037411087df46bc2501d794758e6ca3c16
Spr1ngtr4p07 <didieresneiderm@gmail.com>
2026-03-17 12:00:33 -0500

    Clean up Desafiolibreria.h by removing comments
    Removed commented-out documentation and unnecessary code.

--- Desafio1-2026/Desafiolibreria.h
+++ Desafio1-2026/Desafiolibreria.h
@@ -1,13 +1,5 @@
 #ifndef DESAFIOLIBRERIA_H
 #define DESAFIOLIBRERIA_H
-/*
-<<<<<<< HEAD
-Esta libreria sirve para manejar las funciones del tablero
-y la representacion de las piezas del juego Tetris
-=======
-Esta libreria sirve para.............
->>>>>>> db88b9b8d2022a6ec1db696b3e018cce8633a9cb
-*/
 
 #include <iostream>
 
@@ -24,20 +16,8 @@ int* crearTablero(int alto);
 // alto -> numero de filas
 void imprimirTablero(int* tablero, int ancho, int alto);
 
-<<<<<<< HEAD
-// Estructura para representar una pieza de Tetris
-struct Pieza
-{
-    unsigned int forma[4][4]; // 4 rotaciones y 4 filas
-    int tam;                  // tamaño de la pieza
-};
-
 // Funcion para inicializar las 7 piezas de Tetris
-void inicializarPiezas(Pieza piezas[7]);
 
 #endif // DESAFIOLIBRERIA_H
-=======
 #endif // DESAFIOLIBRERIA_H
 
-
->>>>>>> db88b9b8d2022a6ec1db696b3e018cce8633a9cb
